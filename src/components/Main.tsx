7d264e565ff4f7af1108c2beb61fe879e1d14566
m.ali <majid.live@gmail.com>
2025-11-05 22:41:20 +0330

    v1.1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export const Main = () => {
   return (
-    <div className="flex flex-col justify-center items-center p-8">
+    <div className="flex flex-col justify-center items-center p-8 gap-7">
       <div>
-        <img
-          className="w-[212px] scale-[1.7]"
-          src="./src/assets/img/react.svg"
-          alt=""
-        />
+        <img className="w-[212px] " src="./src/assets/img/react.svg" alt="" />
+      </div>
+      <div>
+        <button className="rounded-2xl border-2 border-[#222] text-[#222] p-2 tracking-widest hover:bg-[#222] hover:text-white">
+          Toggle Menu
+        </button>
       </div>
     </div>
   );
